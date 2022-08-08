ad4ab6c355ea6c65d7966b8e40bb858522b7dff3
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-08 10:35:44 +0200

    Update

--- src/main/java/com/team5/ud22/mvc/vista/ClienteVista.java
+++ src/main/java/com/team5/ud22/mvc/vista/ClienteVista.java
@@ -1,5 +1,20 @@
 package com.team5.ud22.mvc.vista;
-
-public class ClienteVista {
-
+import java.awt.*;
+public class ClienteVista extends JFrame{
+	
+//ATTRIBUTES
+	
+	private JPanel contentPane;
+	private JTextField textFieldIdCliente;
+	
+//CONSTRUCTOR VISTA
+	
+	public ClienteVista() {
+	
+	/* Definition of the window */
+	setTitle("Cliente"); // PopUp window title
+	setBounds(700, 200, 800, 530); // X Y coordinates of the application and its height and length
+	setDefaultCloseOperation(EXIT_ON_CLOSE); // Close the window  when the operation is close
+	setVisible(true); // It allows the window to be visible
+	}
 }
