f62d01d3e35e9b3d3509e38c247a633ab1905564
Joana <joanajanefdez@gmail.com>
2022-08-08 15:12:35 +0200

    cdao.eliminarCliente

--- src/main/java/com/team5/ud22/mvc/App.java
+++ src/main/java/com/team5/ud22/mvc/App.java
@@ -4,10 +4,6 @@ import com.team5.ud22.mvc.modelo.Cliente;
 import com.team5.ud22.mvc.modelo.ClienteDAO;
 import com.team5.ud22.mvc.modelo.conexion.ConnectionDB;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
@@ -19,16 +15,15 @@ public class App
         System.out.println("---ANTES---");
         for(Cliente c : cdao.getClientes()) {
         	System.out.println(c);
-        }
-        
-        //Cliente cliente = new Cliente("marta","garcia","jshvasg","3456g","20220712");
-        //cdao.insertarCliente(cliente);
+        }        
         
         Cliente cliente = cdao.getCliente("3339");
         cliente.setApellido("Vlasdel");
         cliente.setNombre("Ionela");
         // No es pot modificar el dni
         cdao.actualizarCliente(cliente);
+        
+        cdao.eliminarCliente("'3456g'");
         
         System.out.println("\n---DESPUES---");
         for(Cliente c : cdao.getClientes()) {
@@ -41,5 +36,6 @@ public class App
 		ClienteDAO cdao = new ClienteDAO();
 		cdao.insertarCliente(new Cliente("Jone","Joana","Tgn","7778","20220101"));
 		cdao.insertarCliente(new Cliente("Arnau","Mas","Alcover","3339","20020926"));
+		cdao.insertarCliente(new Cliente("Marta","garcia","Bcn","3456g","20220712"));
 	}
 }
