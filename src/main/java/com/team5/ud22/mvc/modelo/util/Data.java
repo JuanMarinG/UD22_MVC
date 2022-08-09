f2c8f4339038a808beacb3ec6d53878accd7e9d9
Joana <joanajanefdez@gmail.com>
2022-08-09 18:02:11 +0200

    changePanelForm()

--- src/main/java/com/team5/ud22/mvc/modelo/util/Data.java
+++ src/main/java/com/team5/ud22/mvc/modelo/util/Data.java
@@ -1,5 +1,21 @@
 package com.team5.ud22.mvc.modelo.util;
 
+import javax.swing.JPanel;
+
+import com.team5.ud22.mvc.vista.ClienteVista;
+
 public class Data {
+	
+	ClienteVista cVista;
+	
+	public void changePanelForm(JPanel panel) {		
+		cVista.getPanel().removeAll();
+		cVista.getPanel().repaint();
+		cVista.getPanel().revalidate();
+		
+		cVista.getPanel().add(panel);
+		cVista.getPanel().repaint();
+		cVista.getPanel().revalidate();		
+	}
 
 }
