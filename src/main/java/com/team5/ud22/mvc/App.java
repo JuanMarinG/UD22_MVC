7e5f55b99d44f64d698f6dd4cef7a2b96846ea04
Joana <joanajanefdez@gmail.com>
2022-08-11 00:20:01 +0200

    pruebas

--- src/main/java/com/team5/ud22/mvc/App.java
+++ src/main/java/com/team5/ud22/mvc/App.java
@@ -2,13 +2,12 @@ package com.team5.ud22.mvc;
 
 import com.team5.ud22.mvc.controlador.Ex1Controlador;
 import com.team5.ud22.mvc.controlador.Ex3Controlador;
-import com.team5.ud22.mvc.controlador.Ex3ControladorProyecto;
 
 public class App {
 	
     public static void main(String[] args) {
     	// Instanciar controlador
-    	new Ex3ControladorProyecto();
+    	new Ex3Controlador();
     }
 
 }
