f57e0f5527789b49bacd8a504c0a7be79fa32e5a
Joana <joanajanefdez@gmail.com>
2022-08-16 02:09:27 +0200

    iniciar clase

--- src/main/java/com/team5/ud22/mvc/controlador/VideoControlador.java
+++ src/main/java/com/team5/ud22/mvc/controlador/VideoControlador.java
@@ -1,5 +1,17 @@
 package com.team5.ud22.mvc.controlador;
 
+import com.team5.ud22.mvc.vista.VideoVista;
+
 public class VideoControlador {
+	
+	private VideoVista videoVista;
+	
+	public VideoControlador() {
+		videoVista = new VideoVista();
+	}
+	
+	public void display() {
+		videoVista.setVisible(true);
+	}
 
 }
